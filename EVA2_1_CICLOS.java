fa71c51c284d1d977634be9b8e0ba93c09fef90b
EvelynAnetteDuarteLopez <L24550849@chihuahua2.tecnm.mx>
2024-10-03 21:53:57 -0600

    Add files via upload

---
+++ EVA2_1_CICLOS.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_1_ciclos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_1_CICLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //ciclo for -> cuando sabemos cuantas veces se repite la instrucion
+        //FUNCIONA COMO CONTADOR: INICIO, FIN (condicion)
+        //i--> index ---> indice
+        //vamos a repetir 10 veces. contando de 1 a 10
+        //NOSOTRO ESTABLECEMOS EL PUNTO DE INICIO Y DE FIN
+        for(int i = 0; i <= 9; i++){
+            System.out.println(i + "-");
+        }
+        System.out.println("");
+        for(int i = 10; i >= 1; i--){
+            System.out.println(i + "-");
+ 
+        }
+            
+            
+    }
+    
+}
